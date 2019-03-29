0672eca7db4b29da79c08bfc8631bddd2c6be05c
allerria <wtf496@gmail.com>
2019-03-30 02:30:49 +0900

    keks.

--- backend/__main__.py
+++ backend/__main__.py
@@ -2,5 +2,5 @@ from api import *
 
 if __name__ == '__main__':
     db.bind(**app.config['PONY'])
-    db.generate_mapping(create_tables=True)
+    db.generate_mapping()
     app.run(host='0.0.0.0')
